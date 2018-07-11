f331fd86ce88177e0380369c56575700dbcd42c7
Vision-X <michael.s.marlow@gmail.com>
2018-07-10 18:09:52 -0600

    completes yeezy web scraper and deployment of API

---
+++ README.md
@@ -0,0 +1,37 @@
+# Web Scraper build - Twitch live code
+
+## A scraper for shoe release dates
+
+### Build checklist
+
+[X] basic setup (dependencies, libraries, etc)
+  - Express
+  - Cors
+  - Request
+  - Cheerio.js
+[X] import cheerio
+[X] accessing the DOM
+  - dev tools
+  - capturing elements we care about
+  - store their sub data
+[X] store data in an Object (lets build the object out thoughtfully)
+  - build output JSON Object
+  - make it useable, thoughtful design
+[X] fully scrape the page for what we want
+  - structure a loop to scan the page
+  - identify/store our data
+[] host the data (an API)
+  - export JSON output
+  - host on firebase
+  - access from firebase to confirm
+[] bonus time: automating the scraper to run 1 time a day
+  - use a task scheduler to run our Scraper
+  - talk about other options
+  - maintaining new daily data to hosted API
+
+  extra extra...
+
+  [] - looking into meta properties for SEO: can give pointers to
+        what is what on the page
+  [] - parsers for each site?
+  [] -
---
+++ index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var cors = require('cors');
+var app = express();
+
+var json = {};
+
+app.get('/scrape', function(req, res) {
+  // console.log(req.query.name);
+  console.log("HIYA");
+  var url = 'https://sneakernews.com/2018/07/03/adidas-yeezy-fall-2018-release-info/';
+
+request(url, function(error, response, html) {
+  if (!error) {
+    console.log("no error...");
+    var $ = cheerio.load(html);
+    var shoeImgs = [];
+    var shoeDetails = [];
+
+    $('.latest_posts').siblings().each(function(i, elem) {
+      if (elem.name === 'p' && $(this).siblings('div')) {
+        if ($(this).children('img')) {
+          var imgSrc = $(this).children('img').attr('src');
+          // console.log(imgSrc, "... imgSrc");
+          shoeImgs.push(imgSrc);
+        }
+      }
+    })
+
+    $('.rel-full').children().children().each(function(i, elem) {
+      var relDate = $(this).children().eq(2).text().slice(14);
+      var name = $(this).children().eq(0).text();
+      var price = $(this).last().text();
+      var shortPrice = '$' + price.substr(price.length - 3);
+      shoeDetails.push([name, relDate, shortPrice]);
+    })
+
+    shoeImgs.shift();
+
+    for (let i = 0; i < shoeImgs.length; i++) {
+      json[i] = {};
+      json[i].imgUrl = shoeImgs[i];
+    };
+
+    for (let i = 0; i < shoeDetails.length; i++) {
+      json[i].name = shoeDetails[i][0];
+      json[i].relDate = shoeDetails[i][1];
+      json[i].price = shoeDetails[i][2];
+    };
+  }
+
+  fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+    console.log('File successfully written! - Check your project directory for the output.json file!');
+  })
+
+  res.send("yo did it")
+})
+
+})
+
+app.get('/', function(req, res) {
+  res.json({
+    message: "hello you are not going crazy"
+  })
+})
+
+app.listen('4000');
+console.log('Listening on port 4000');
