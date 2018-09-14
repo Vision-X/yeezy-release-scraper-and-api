8d4b3058f91f31480af7dc4bc9c655542d6078ea
Vision-X <michael.s.marlow@gmail.com>
2018-09-14 13:41:32 -0600

    add logger for yeezy scrape functionality

--- index.js
+++ index.js
@@ -51,6 +51,8 @@ request(url, function(error, response, html) {
     };
   }
 
+  console.log("shoe details array...", shoeDetails);
+
   fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
     console.log('File successfully written! - Check your project directory for the output.json file!');
   })
